adfddb8063c8fa1dbc7496c7ef8be3b5cde4dbf4
kburger <burger.github@gmail.com>
2017-04-03 23:15:10 +0200

    Added css jungle test for expression preview check

---
+++ src/test/groovy/nl/dtls/fairifier/acceptance/ExpressionPreviewDialogSpec.groovy
@@ -0,0 +1,48 @@
+package nl.dtls.fairifier.acceptance
+
+import geb.spock.GebReportingSpec
+
+class ExpressionPreviewDialogSpec extends GebReportingSpec {
+    def "navigate to preview dialog and check the expected value"() {
+        when:
+        go "/"
+        $("li.action-area-tab", 0).click()
+        report "create project tab"
+        
+        and:
+        $("div.create-project-ui-source-selection-tab", 2).click()
+        report "clipboard tab"
+        
+        and:
+        $("textarea#default-importing-clipboard-textarea") << """\
+            id,name,value
+            1,test,17
+            2,foo,42
+            3,bar,94
+            """.stripIndent()
+        report "text input"
+        
+        and:
+        $("div.create-project-ui-source-selection-tab-body.selected form button[bind=nextButton]").click()
+        waitFor { $("table.data-table") }
+        report "form submit"
+        
+        and:
+        $("div.default-importing-wizard-header button[bind=nextButton]").click()
+        waitFor { $("div.data-table-container") }
+        report "data-imported"
+        
+        and:
+        $("span.button-menu", text: "RDF").click()
+        report "rdf menu opened"
+        $("div.menu-container a.menu-item", 0).click()
+        report "edit semantic model dialog opened"
+        $("a", text: "(row index) URI").click()
+        report "row uri dialog opened"
+        $("a[bind=rdf_cell_expr_preview]").click()
+        report "preview dialog opened"
+        
+        then:
+        $("div.expression-preview-container").$("tr", 1).$("td", 3).text() == "http://fairifier-dev.fair-dtls.surf-hosted.nl/0"
+    }
+}
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,7 +1,9 @@
+import org.openqa.selenium.Dimension
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 
 driver = {
     def phantomjs = new PhantomJSDriver(new DesiredCapabilities())
+    phantomjs.manage().window().setSize(new Dimension(1024, 768))
     phantomjs
 }
